12e5a26805970a6989f87d31cde9007799a52900
Ryan Young <ryanbyoung1@gmail.com>
2023-01-09 16:55:47 -0500

    wrapped switch case in a function and created init function with prompt

--- server.js
+++ server.js
@@ -8,24 +8,47 @@ const db = mysql.createConnection({
     database: "employee_db",
 });
 
-switch(type) {
-    case 'VIEW ALL EMPLOYEES': {
-        db.query('SELECT * FROM employee', (err, employees) => {
-            console.table(employees);
-        });
-        break;
+const chooseOption = (type) => {
+    switch(type) {
+        case 'VIEW ALL EMPLOYEES': {
+            db.query('SELECT * FROM employee', (err, employees) => {
+                console.table(employees);
+                init();
+            });
+            break;
+        }
+        case 'VIEW ALL DEPARTMENTS': {
+            db.query('SELECT * FROM department', (err, departments) => {
+                console.table(departments);
+                init();
+            });
+            break;
+        }
+        case 'VIEW ALL ROLES': {
+            db.query('SELECT * FROM role', (err, roles) => {
+                console.table(roles);
+                init();
+            });
+            break;
+        }
     }
-    case 'VIEW ALL DEPARTMENTS': {
-        db.query('SELECT * FROM department', (err, departments) => {
-            console.table(departments);
-        });
-        break;
-    }
-    case 'VIEW ALL ROLES': {
-        db.query('SELECT * FROM role', (err, roles) => {
-            console.table(roles);
-        });
-        break;
-    }
-}
+};
+
+const init = () => {
+    prompt({
+        type: 'rawlist',
+        message: 'What would you like to do?',
+        choices: [
+            'VIEW ALL EMPLOYEES',
+            'VIEW ALL DEPARTMENTS',
+            'VIEW ALL ROLES',
+        ],
+        name: 'type',
+    })
+    .then((answer) => {
+        chooseOption(answer.type);
+    });
+};
+
+init();
 
